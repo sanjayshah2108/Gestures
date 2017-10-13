55e91a539d7f061ea30ba45b4d3bdb467081ab6d
Sanjay Shah <Sanjay Shah>
2017-10-12 17:23:44 -0700

    Initial Commit

---
+++ Gestures/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  Gestures
+//
+//  Created by Sanjay Shah on 2017-10-12.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
